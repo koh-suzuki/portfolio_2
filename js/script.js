7cccbdf21e86290a3741eac1ca3ae2e2a79f6b9f
aoshiro <koh.sss0629@gmail.com>
2021-03-25 22:20:50 +0900

    ヘッダー背景切り替えとスライダー　追加

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+  var target = $("main");
+  var el = target.offset().top;
+  $(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > el) {
+      $(".header__wrapper").addClass("js__scroll__head");
+    } else {
+      $(".header__wrapper").removeClass("js__scroll__head");
+    }
+  });
+});
